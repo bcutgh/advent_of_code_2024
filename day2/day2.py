a59d7c546d2e8199daa871f0d2241400a3e93311
Brad Cutler <bcutler@easydns.com>
2025-09-27 11:42:05 -0400

    Day 2, part 2 is complete. Lots of debug output added for this one. The tricky part was to try dampening when any of the initial attempts to validate a report failed, not just when all of them fail.

--- day2/day2.py
+++ day2/day2.py
@@ -13,7 +13,7 @@ def adjacent_levels_within_tolerance(report):
         report (string): A string of levels to check.
 
     Returns:
-        bool: True if all levels are within tolerance.
+        bool: True if all levels are within tolerance, otherise False.
     """
     levels = report.split()
     count = 0
@@ -43,6 +43,36 @@ def adjacent_levels_within_tolerance(report):
     print(f"Acceptable: {report}")
     return True
 
+def dampen(report):
+    """
+    Remove at most one bad record from a report and retest it.
+
+    Args:
+        report (string): A string of levels to work with.
+
+    Returns:
+        bool: True if dampening was successful.
+    """
+    separator = ' '
+    original_report = str(report)
+    original_levels = original_report.split()
+    num_levels = len(original_levels)
+    for index_to_delete in range(num_levels):
+        test_levels = original_levels.copy()
+        print(f"Testing a dampening by removing index {index_to_delete} from {original_report} which is {test_levels[index_to_delete]}")
+        del test_levels[index_to_delete] # Remove one level from the report
+        dampened_report = separator.join(test_levels)
+        if levels_decreasing(dampened_report):
+            if (adjacent_levels_within_tolerance(dampened_report)):
+                print(f"Dampening successful for descreasing {dampened_report}")
+                return True
+        elif levels_increasing(dampened_report):
+            if (adjacent_levels_within_tolerance(dampened_report)):
+                print(f"Dampening successful for increasing {dampened_report}")
+                return True
+        print(f"Dampening was not successful for {original_report}")
+    return False
+
 def levels_decreasing(report):
     """
     Check if all the levels in a report are decreasing
@@ -54,10 +84,10 @@ def levels_decreasing(report):
         bool: True if all levels are decreasing.
     """
     levels = report.split()
-    previous_value = 1000000
+    previous_value = 1000000 # Set a high/valid start value
     for level in levels:
         if int(level) >= previous_value:
-            print(f"NOT decreasing {level}: {report}")
+            print(f"NOT decreasing due to {level} in {report}")
             return False
         previous_value = int(level)
     print(f"Decreasing: {report}")
@@ -77,7 +107,7 @@ def levels_increasing(report):
     previous_value = 0
     for level in levels:
         if (int(level) <= previous_value):
-            print(f"Not increasing {level}: {report}")
+            print(f"Not increasing due to {level} in {report}")
             return False
         previous_value = int(level)
     print(f"Increasing: {report}")
@@ -107,12 +137,19 @@ def main():
     """
     reports = read_input_file("puzzle_input.txt")
     num_safe_reports = 0
+    safe_report_found = False
     for report in reports:
         if levels_decreasing(report):
             if (adjacent_levels_within_tolerance(report)):
                 num_safe_reports += 1
+                safe_report_found = True
         elif levels_increasing(report):
             if (adjacent_levels_within_tolerance(report)):
+                num_safe_reports += 1
+                safe_report_found = True
+        if not safe_report_found:
+            dampened_report_success = dampen(report)
+            if dampened_report_success:
                 num_safe_reports += 1
 
     print(f"The number of safe reports is: {num_safe_reports}")
