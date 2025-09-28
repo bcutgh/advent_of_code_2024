456ec0d32421f5c57b2bb721ea9f80b021025398
Brad Cutler <bcutler@easydns.com>
2025-09-28 10:33:34 -0400

    Day 3, part 1 complete

--- day3/day3.py
+++ day3/day3.py
@@ -3,6 +3,8 @@
 # Date: September 27, 2025
 # Description: Advent of Code 2024 Day 3
 
+import re # We'll need the regular expression library
+
 def read_input_file(file_name):
     """
     Read data from a file.
@@ -15,6 +17,8 @@ def read_input_file(file_name):
     """
     with open(file_name, "r") as file:
         instruction_set = file.readlines()
+        instruction_set = str(instruction_set)
+        instruction_set.strip() # Remove leading/trailing whitespace/newlines
         return instruction_set
 
 def main():
@@ -23,6 +27,17 @@ def main():
     """
     instruction_set = read_input_file("puzzle_input.txt")
     print(f"The instruction_set is: {instruction_set}")
+
+    regex_ignore_corrupt_values = r"mul\((\d{1,3}),(\d{1,3})\)"
+    valid_mul_instructions = re.findall(regex_ignore_corrupt_values, instruction_set)
+    print(f"\nThe valid mul instructions are: {valid_mul_instructions}")
+
+    result = 0
+    for x, y in valid_mul_instructions:
+        print(f"X: {x}")
+        print(f"Y: {y}")
+        result += int(x) * int(y)
+    print(f"The result is {result}")
 
 if __name__ == "__main__":
     main()
